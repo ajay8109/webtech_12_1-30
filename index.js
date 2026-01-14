63adb336c514ae2fb3da8571e3e7664964017a4f
ajay8109 <ajayverma81099@gmail.com>
2026-01-14 13:03:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function fun() {
+
+
+
+let a = 20;
+ for(i = 0; i <= a; i++) {
+    console.log(i);
+ }
+
+}
+
+fun();
---
+++ js basic/demo.js
@@ -0,0 +1,30 @@
+function add() {
+    let a = 20;
+    let b= 30;
+
+    console.log(a+b);
+}
+
+add();
+
+
+function add() {
+    let a = 20;
+    let b = 20;
+
+    return a + b;
+}
+
+let result = add();
+
+console.log(result);
+
+
+function add() {
+     let a = 20;
+     let b = 30;
+
+     let result = a + b;
+}
+
+add();
